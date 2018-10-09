e2bed80fd53d96876b0b02425ab71f1ef618d09b
wangyuqing1 <1932206997@qq.com>
2018-10-09 20:08:32 +0800

    Post  开发完成

--- post/models.py
+++ post/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created']
--- post/views.py
+++ post/views.py
@@ -1,6 +1,61 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from math import ceil
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render
+from post.models import Post
 
-# Create your views here.
+
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        return render(request, 'create_post.html')
+
+
+def edit_post(request):
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        return render(request, 'edit_post.html', {'post': post})
+
+
+def read_post(request):
+    post_id = int(request.GET.get('post_id'))
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'read_post.html', {'post': post})
+
+
+def delete_post(request):
+    post_id = int(request.GET.get('post_id'))
+    Post.objects.get(pk=post_id).delete()
+    return redirect('/')
+
+
+def post_list(request):
+    page = int(request.GET.get('page', 1))
+    total = Post.objects.count()
+    per_page = 3
+    pages = int(ceil(total / per_page))
+
+    start = (page - 1) * per_page
+    end = start + per_page
+    posts = Post.objects.all()[start:end]
+
+    return render(request, 'post_list.html',
+                  {'posts': posts, 'pages': range(pages)})
+
+
+def search(request):
+    keyword = request.POST.get('keyword')
+    posts = Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts': posts})
